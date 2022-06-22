d9bba4e994623ebe5f9017679a94a6c08c63404f
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2022-06-22 00:23:02 -0300

    Change patterns to array

--- lib/mask.ts
+++ lib/mask.ts
@@ -1,24 +1,33 @@
 import { tokens } from "./tokens";
 import { unmask } from "./unmask";
 
-export const mask = (value: string, pattern: string): string => {
-  if (!value || !pattern) {
-    throw ReferenceError("Pattern or value not found.");
+export const mask = (value: string, patterns: string[]): string => {
+  if (!value || !patterns) {
+    throw ReferenceError("Value or pattern not found.");
   }
+  if (!Array.isArray(patterns)) {
+    throw ReferenceError("Pattern should be an array");
+  }
+
+  const [pattern, dynamicPattern] = patterns.sort(
+    (a, b) => a.length - b.length
+  );
+  const computedPattern =
+    unmask(value).length <= unmask(pattern).length ? pattern : dynamicPattern;
 
   let output = "";
 
   for (
-    let unmaskedValue = unmask(value, pattern),
-      unmaskedPattern = unmask(pattern),
-      patternLength = pattern.length,
+    let unmaskedValue = unmask(value, computedPattern),
+      unmaskedPattern = unmask(computedPattern),
+      patternLength = computedPattern.length,
       i = 0,
       ii = 0;
     i < patternLength && unmaskedValue[ii];
     i++
   ) {
     const token = tokens[unmaskedPattern[ii]],
-      patternChar = pattern[i],
+      patternChar = computedPattern[i],
       inputChar = unmaskedValue[ii];
 
     if (!token.test(inputChar)) break;
